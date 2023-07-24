e825be51c10b3d645db383cac98463bbaf936035
andres mauricio gonzalez <andres.gonzalez@cluvi.com>
2023-07-23 20:36:11 -0500

    aplicacion de todo list

--- src/App.vue
+++ src/App.vue
@@ -8,12 +8,32 @@
   const input_category = ref(null)
 
   const todos_asc = computed(() => todos.value.sort((a,b)=>{
-    return a.createdAt - b.createdAt 
+    return b.createdAt - a.createdAt 
   }))
 
   const addTodo = () =>{
+    if(input_content.value.trim() === '' || input_category.value === null){
+      return
+    }
 
+    todos.value.push({
+      content: input_content.value,
+      category: input_category.value,
+      done:false,
+      createdAt: new Date().getTime()
+    })
+
+    input_content.value = ''
+    input_category.value = null
   }
+
+  const removeTodo = todo =>{
+    todos.value = todos.value.filter(t => t !== todo)
+  }
+
+  watch(todos, newVal => {
+    localStorage.setItem('todos', JSON.stringify(newVal))
+  },{ deep:true })
 
   watch(name, (newVal) => {
     localStorage.setItem('name', newVal)
@@ -21,6 +41,7 @@
 
   onMounted(() => {
     name.value = localStorage.getItem('name') || ''
+    todos.value = JSON.parse(localStorage.getItem('todos')) || []
   })
 </script>
 
@@ -69,6 +90,26 @@
 
           <input type="submit" value="Add todo" />
         </form>
+      </section>
+      
+      <section class="todo-list">
+        <h3>TODO LIST</h3>
+        <div class="list">
+          <div v-for="todo in todos_asc" :class="`todo-item ${todo.done && 'done'}`">
+            <label>
+              <input type="checkbox" v-model="todo.done" />
+              <span :class="`bubble ${todo.category}`" ></span>
+            </label>
+
+            <div class="todo-content">
+              <input type="text" v-model="todo.content" />
+            </div>
+
+            <div class="actions">
+              <button class="delete" @click="removeTodo(todo)">Delete</button>
+            </div>
+          </div>
+        </div>
       </section>
     </main>
 </template>
